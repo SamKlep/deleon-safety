41798e04b242f325be8e5a23ee8894911c7a21e6
SamKlep <shamdemann23@gmail.com>
2021-01-06 09:09:29 -0600

    Form sending data to email.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,57 +1,52 @@
 import React from 'react'
+import emailjs from 'emailjs-com'
 import { Form, Button } from 'react-bootstrap'
 
-const ContactForm = () => {
+export default function ContactUs() {
+  function sendEmail(e) {
+    e.preventDefault()
+
+    emailjs
+      .sendForm('gmail', 'contact_form', e.target, 'user_55YYmU3urijwaiV2s1l3i')
+      .then(
+        (result) => {
+          console.log(result.text)
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+  }
+
   return (
-    <div>
-      <Form>
-        <Form.Group controlId='exampleForm.ControlInput1'>
-          <Form.Label>Company Name</Form.Label>
-          <Form.Control type='text' />
-        </Form.Group>
-        <Form.Group controlId='exampleForm.ControlInput2'>
-          <Form.Label>Address</Form.Label>
-          <Form.Control type='text' />
-        </Form.Group>
-        <Form.Group controlId='exampleForm.ControlInput3'>
-          <Form.Label>City</Form.Label>
-          <Form.Control type='text' />
-        </Form.Group>
-        <Form.Group controlId='exampleForm.ControlInput4'>
-          <Form.Label>State/Province</Form.Label>
-          <Form.Control type='text' />
-        </Form.Group>
-        <Form.Group controlId='exampleForm.ControlInput5'>
-          <Form.Label>Zip/Postal Code</Form.Label>
-          <Form.Control type='text' />
-        </Form.Group>
-        <Form.Group controlId='exampleForm.ControlInput6'>
-          <Form.Label>Country</Form.Label>
-          <Form.Control type='text' />
-        </Form.Group>
-        <Form.Group controlId='exampleForm.ControlInput7'>
-          <Form.Label>Name</Form.Label>
-          <Form.Control type='text' />
-        </Form.Group>
-        <Form.Group controlId='exampleForm.ControlInput8'>
-          <Form.Label>Email</Form.Label>
-          <Form.Control type='email' placeholder='name@example.com' />
-        </Form.Group>
-        <Form.Group controlId='exampleForm.ControlSelect9'>
-          <Form.Label>Requested Service</Form.Label>
-          <Form.Control as='select'>
-            <option>Training</option>
-            <option>Consulting</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group controlId='exampleForm.ControlTextarea10'>
-          <Form.Label>Comments/Questions</Form.Label>
-          <Form.Control as='textarea' rows={3} />
-        </Form.Group>
-        <Button>Submit</Button>
-      </Form>
-    </div>
+    <Form className='contact-form' onSubmit={sendEmail}>
+      <Form.Group>
+        <Form.Control type='hidden' name='contact_number' />
+        <Form.Label>Name</Form.Label>
+        <Form.Control type='text' name='from_name' />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Address</Form.Label>
+        <Form.Control as='textarea' name='user_os' rows={3} />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Email</Form.Label>
+        <Form.Control type='email' name='user_email' />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Requested Service</Form.Label>
+        <Form.Control as='select' name='user_ip'>
+          <option>Training</option>
+          <option>Consulting</option>
+        </Form.Control>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Message</Form.Label>
+        <Form.Control as='textarea' name='message' rows={3} />
+      </Form.Group>
+      <Button variant='primary' value='Send' type='submit'>
+        Submit
+      </Button>
+    </Form>
   )
 }
-
-export default ContactForm
