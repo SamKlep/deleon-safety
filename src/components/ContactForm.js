875253be8f42f7325a41f94d3cb07d8fc5d583cc
SamKlep <shamdemann23@gmail.com>
2021-01-06 11:00:10 -0600

    Form clears after its sent.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,6 +2,15 @@ import React from 'react'
 import emailjs from 'emailjs-com'
 import { Form, Button } from 'react-bootstrap'
 
+function fun() {
+  document.getElementById('from_name').value = ''
+  document.getElementById('user_platform').value = ''
+  document.getElementById('user_os').value = ''
+  document.getElementById('user_email').value = ''
+  document.getElementById('user_ip').value = ''
+  document.getElementById('message').value = ''
+}
+
 export default function ContactUs() {
   function sendEmail(e) {
     e.preventDefault()
@@ -16,33 +25,38 @@ export default function ContactUs() {
           console.log(error.text)
         }
       )
+    fun()
   }
 
   return (
-    <Form className='contact-form' onSubmit={sendEmail}>
+    <Form id='contact-form' className='contact-form' onSubmit={sendEmail}>
       <Form.Group>
         <Form.Control type='hidden' name='contact_number' />
         <Form.Label>Company Name</Form.Label>
-        <Form.Control type='text' name='from_name' />
+        <Form.Control id='from_name' type='text' name='from_name' />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Phone</Form.Label>
+        <Form.Control id='user_platform' type='text' name='user_platform' />
       </Form.Group>
       <Form.Group>
         <Form.Label>Address</Form.Label>
-        <Form.Control as='textarea' name='user_os' rows={3} />
+        <Form.Control id='user_os' as='textarea' name='user_os' rows={3} />
       </Form.Group>
       <Form.Group>
         <Form.Label>Email</Form.Label>
-        <Form.Control type='email' name='user_email' />
+        <Form.Control id='user_email' type='email' name='user_email' />
       </Form.Group>
       <Form.Group>
         <Form.Label>Requested Service</Form.Label>
-        <Form.Control as='select' name='user_ip'>
+        <Form.Control id='user_ip' as='select' name='user_ip'>
           <option>Training</option>
           <option>Consulting</option>
         </Form.Control>
       </Form.Group>
       <Form.Group>
         <Form.Label>Message</Form.Label>
-        <Form.Control as='textarea' name='message' rows={3} />
+        <Form.Control id='message' as='textarea' name='message' rows={3} />
       </Form.Group>
       <Button variant='primary' value='Send' type='submit'>
         Submit
